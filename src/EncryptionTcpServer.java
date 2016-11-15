bf8881c115b9bec7bcb60a8d28f9e3d3da31db81
lumphrey <vincentchu3@gmail.com>
2016-11-15 02:08:25 -0800

    Cleanups.

--- src/EncryptionTcpServer.java
+++ src/EncryptionTcpServer.java
@@ -1,6 +1,3 @@
-import com.sun.corba.se.spi.activation.Server;
-import com.sun.istack.internal.Nullable;
-
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -17,7 +14,7 @@ public class EncryptionTcpServer {
     //for use in Diffie-Hellman key exchange
     private static int mG = 5; //base
     private static int mP = 23; //modulus
-    private static int mLocalSecret = 6;
+    private static int mLocalSecret = 6; //TODO: randomize
     private static int mSecretKey;
 
     //sockets
@@ -33,7 +30,7 @@ public class EncryptionTcpServer {
      */
     public static void main(String[] args) {
 
-        mSecretKey = testComputerSecret();
+        mSecretKey = testComputeSecret();
         System.out.println("Secret computed: " + mSecretKey);
 
         //initialization
@@ -42,10 +39,13 @@ public class EncryptionTcpServer {
         listen();
         initalizeStreams();
 
+
+        //wait for client's "special number", needed to calculate secret key
         String message = waitForUnencryptedMessage();
         int clientNum = -1;
         try {
             clientNum = Integer.parseInt(message);
+            System.out.println("Received integer " + clientNum + " from client. Calculating key...");
         }
         catch (NumberFormatException e) {
             System.out.println("Expected integer from client. Closing connection.");
@@ -58,7 +58,14 @@ public class EncryptionTcpServer {
         }
 
         mSecretKey = calculateSecret(clientNum);
+        System.out.println("Calculated secret key: " + mSecretKey);
 
+        //send server's "special number" to client
+        int serverNum = (int) (Math.pow(mG, mLocalSecret) % mP);
+        System.out.println("Sending int " + serverNum + " to client...");
+        sendUnencryptedMessage(Integer.toString(serverNum));
+
+        //wait for "ready" message from client before starting secure transfers
 
 
     }
@@ -71,7 +78,7 @@ public class EncryptionTcpServer {
     private static void extractArgs(String[] args) {
 
         if(args.length < 2) {
-            System.out.println("Incorrect number of parameters. Required: port, total number of security schemes.");
+            System.out.println("Incorrect number of parameters. Required: destination host, port, total number of security schemes.");
             System.exit(-1);
         }
 
@@ -98,6 +105,7 @@ public class EncryptionTcpServer {
     private static void initializeServer() {
         try {
             mServerSocket = new ServerSocket(mPort);
+            System.out.println("Server listening on port " + mPort);
         }
         catch (IOException e) {
             System.out.println("Error initializing server on port " + mPort);
@@ -131,6 +139,7 @@ public class EncryptionTcpServer {
 
         try {
             mToClient = new DataOutputStream(mClient.getOutputStream());
+            System.out.println("IO streams initialized.");
         }
         catch (IOException e) {
             System.out.println("Error initializing output stream to client.");
@@ -158,8 +167,10 @@ public class EncryptionTcpServer {
         String message = null;
 
         while(true) {
+            System.out.println("Waiting for message...");
             try {
                 message = mReader.readLine();
+                System.out.println("Message received: " + message);
             }
             catch (IOException e) {
                 System.out.println("Error receiving message.");
@@ -175,6 +186,17 @@ public class EncryptionTcpServer {
         return message;
     }
 
+    private static void sendUnencryptedMessage(String msg) {
+        try {
+            mToClient.writeBytes(msg);
+        }
+        catch (IOException e) {
+            System.out.println("Error sending message to client.");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
 
     /**
      * Calculates a secret key given a special number from the client.
@@ -184,16 +206,16 @@ public class EncryptionTcpServer {
     private static int calculateSecret(double clientNum) {
         int secret = -1;
 
-        secret = (int) (Math.pow(clientNum, (double)mLocalSecret) % mP);
+        secret = (int) (Math.pow(clientNum, mLocalSecret) % mP);
 
         return secret;
     }
 
 
-    private static int testComputerSecret() {
+    private static int testComputeSecret() {
         int secret = -1;
 
-        secret = (int) (Math.pow(19, mLocalSecret) % mP);
+        secret = (int) (Math.pow(8, 15) % mP);
 
         return secret;
     }
